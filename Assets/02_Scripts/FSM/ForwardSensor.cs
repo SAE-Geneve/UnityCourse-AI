ed56cf8eafb10b1ca744f6aaaa2df3f5d9819183
StudioAlbert <account@studio-albert.com>
2025-03-12 14:22:30 +0100

    12/03/2025 course

---
+++ Assets/02_Scripts/FSM/ForwardSensor.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ForwardSensor : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    [SerializeField] private float angleDetection = 45;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private string tag;
+
+    public bool HasDetected;
+    private Vector3 hitPosition;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = HasDetected ? Color.green : Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0, angleDetection, 0) * transform.forward * radius);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0, -angleDetection, 0) * transform.forward * radius);
+        
+        Gizmos.DrawLine(transform.position, hitPosition);
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        HasDetected = false;
+        hitPosition = transform.position;
+        
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius, layerMask);
+        
+        Collider goodObject = colliders.FirstOrDefault(c => c.CompareTag(tag));
+        if (goodObject != null)
+        {
+            Vector3 goodObjectDistance = goodObject.bounds.center - transform.position;
+            float angle = Vector3.Angle(transform.forward, goodObjectDistance);
+            
+            if(angle < angleDetection)
+            {
+                if(Physics.Raycast(transform.position, goodObjectDistance, out RaycastHit hit, radius, layerMask))
+                {
+                    hitPosition = hit.point;
+                    if(hit.collider == goodObject) HasDetected = true;
+                }
+            }
+        }
+
+    }
+}
