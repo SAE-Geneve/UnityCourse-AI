6140046d688b2d29a3fbc776a37850bf53b16f83
StudioAlbert <account@studio-albert.com>
2025-03-20 13:32:25 +0100

    BFS/DFS Search

--- Assets/02_Scripts/Pathfinding/Pathfinding.cs
+++ Assets/02_Scripts/Pathfinding/Pathfinding.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -8,7 +9,11 @@ public class Pathfinding : MonoBehaviour
 
     [SerializeField] private Tilemap ground;
     [SerializeField] private Tilemap debug;
+    [SerializeField] private Tilemap doors;
     [SerializeField] private TileBase debugTile;
+
+    [SerializeField] private Sprite closed;
+    [SerializeField] private Sprite opened;
 
     [SerializeField] private Transform entry;
     [SerializeField] private Transform treasure;
@@ -35,37 +40,44 @@ public class Pathfinding : MonoBehaviour
             Debug.Log("Treasure on the map");
         else
             Debug.Log("Treasure NOT on the map");
-        
-        BFS_Search();
-        
+
+        //BFS_Search();
+        //DFS_Search();
+
+        //StartCoroutine("BFS_Search");
+        //debug.ClearAllTiles();
+        StartCoroutine("DFS_Search");
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
 
     }
-    
-    private void BFS_Search()
+
+    private IEnumerator BFS_Search()
     {
 
         Vector3Int startPosition = ground.WorldToCell(entry.position);
         Queue<Vector3Int> q = new Queue<Vector3Int>();
         List<Vector3Int> visited = new List<Vector3Int>();
 
+        bool foundTarget = false;
+
         q.Enqueue(startPosition);
-        
+
         do
         {
 
             startPosition = q.Dequeue();
-            
+
             // Debug
             debug.SetTile(startPosition, debugTile);
-            
-            if(!visited.Contains(startPosition))
+            yield return new WaitForSeconds(0.05f);
+
+            if (!visited.Contains(startPosition))
             {
                 visited.Add(startPosition);
             }
@@ -73,18 +85,20 @@ public class Pathfinding : MonoBehaviour
             foreach (Vector3Int neighbour in _neighbours)
             {
                 Vector3Int newPos = startPosition + neighbour;
-                
+
                 if (newPos == ground.WorldToCell(treasure.position))
                 {
                     Debug.Log($"Trouvé : {newPos}");
-                    return;
+                    foundTarget = true;
+                    continue;
+                    //yield return new WaitForEndOfFrame();
                 }
-                
-                if(!q.Contains(newPos))
+
+                if (!q.Contains(newPos))
                 {
                     if (!visited.Exists(v => v.x == newPos.x && v.y == newPos.y && v.z == newPos.z))
                     {
-                        if(ground.HasTile(newPos))
+                        if (ground.HasTile(newPos))
                         {
                             q.Enqueue(newPos);
                         }
@@ -92,9 +106,75 @@ public class Pathfinding : MonoBehaviour
                 }
             }
 
-        } while (q.Count > 0);
-        
+        } while (q.Count > 0 && foundTarget == false);
+
         Debug.Log($"Pas Trouvé :(");
-        
+
     }
+    private IEnumerator DFS_Search()
+    {
+
+        Vector3Int startPosition = ground.WorldToCell(entry.position);
+        Stack<Vector3Int> q = new Stack<Vector3Int>();
+        List<Vector3Int> visited = new List<Vector3Int>();
+
+        bool foundTarget = false;
+
+        q.Push(startPosition);
+
+        do
+        {
+
+            startPosition = q.Pop();
+
+            // Debug
+            debug.SetTile(startPosition, debugTile);
+            yield return new WaitForSeconds(0.05f);
+
+            if (!visited.Contains(startPosition))
+            {
+                visited.Add(startPosition);
+            }
+
+            foreach (Vector3Int neighbour in _neighbours)
+            {
+                Vector3Int newPos = startPosition + neighbour;
+
+                if (newPos == ground.WorldToCell(treasure.position))
+                {
+                    Debug.Log($"Trouvé : {newPos}");
+                    foundTarget = true;
+                }
+
+                if (!q.Contains(newPos))
+                {
+                    if (!visited.Exists(v => v.x == newPos.x && v.y == newPos.y && v.z == newPos.z))
+                    {
+                        if (ground.HasTile(newPos))
+                        {
+                            if (doors.HasTile(newPos))
+                            {
+                                Sprite doorSprite = doors.GetSprite(newPos);
+                                if (doorSprite == opened)
+                                {
+                                    q.Push(newPos);
+                                } 
+                                
+                            }else
+                            {
+                                q.Push(newPos);
+                            }
+                        }
+                    }
+                }
+            }
+
+        } while (q.Count > 0 && foundTarget == false);
+
+        Debug.Log($"Pas Trouvé :(");
+
+    }
+
+
+
 }
